4e298d4fa70cec82c2177689c61949ca105ca16d
dagyrox <dagyrox@gmail.com>
2016-04-03 13:48:04 -0400

    Store working skeleton
    Completed the working skeleton for the stores.  Can now insert a new
    store.
    
    The next idea is to get a working skeleton for items working.  The idea
    for items model is simply name and descriptions.  But we'll want to have
    a different kind of item, possibly named list item or store item, which
    will contain the item linked to a store and a price.  That way when
    adding an item to a shopping cart, we'll be adding a list itemw hich
    will already contain the store and the price.

--- client/app/stores/stores.controller.js
+++ client/app/stores/stores.controller.js
@@ -1,11 +1,41 @@
 'use strict';
 
 angular.module('myShoppinglistApp')
-  .controller('StoresCtrl', function ($scope) {
-  	var vm = this;
+.controller
+(
+	'StoresCtrl'
+	, function ($http) 
+	{
+	  	var vm = this;
 
-  	vm.NewAdd = NewAdd;
+	  	vm.NewAdd = NewAdd;
+	  	vm.AddSore = AddStore;
 
 
-  	function NewAdd(){alert('hi');}
-  });
+	  	function NewAdd()
+	  	{
+	  		var newStore = {name: 	vm.newName,
+							info: 	vm.newDescription,
+							street: 	vm.newStreet,
+							city: 	vm.newCity,
+							state: 	vm.newState,
+							geocode: vm.newGeocode,
+							active: 	true};
+
+			AddStore(newStore);
+
+			vm.newName = "";
+			vm.newDescription = "";
+			vm.newStreet = "";
+			vm.newCity = "";
+			vm.newState = "";
+			vm.newGeocode = "";
+	  	}
+
+		function AddStore(data)
+		{
+			$http.post('/api/stores', data);
+		};
+
+  	}
+);
